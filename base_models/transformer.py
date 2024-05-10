12b26f0725fc93b9905950b350de65a0f5c23348
VisH317 <vt201916384@gmail.com>
2024-05-10 07:52:39 -0400

    adding custom trainable poolers

--- base_models/transformer.py
+++ base_models/transformer.py
@@ -1,21 +1,42 @@
 import torch
+from torch import nn, Tensor
+from protCLIP.utils import RMSNorm
 from transformers import AutoModel, AutoTokenizer
 from typing import List
+
 
 # these are base BERT models to test with at first. These are being used for their lower compuational complexity and matching with original KeAP architecture
 text_model_id = "microsoft/BiomedNLP-BiomedBERT-base-uncased-abstract-fulltext"
 prot_model_id = "yarongef/DistilProtBert"
 
+
+class BertPooler(nn.Module):
+    def __init__(self, d_model: int, use_norm: bool = False) -> None:
+        super().__init__()
+
+        self.lin = nn.Linear(d_model, d_model)
+        self.act = nn.Tanh()
+        self.use_norm = use_norm
+
+        if self.use_norm: self.norm = RMSNorm(d_model)
+
+    def forward(self, hidden_states: Tensor) -> Tensor:
+        if self.use_norm: hidden_states = self.norm(hidden_states)
+        return self.act(self.lin(hidden_states[:, 0]))
+
+
 class BertModel:
-    def __init__(self, model_id, max_len: int = 2048):
+    def __init__(self, model_id, max_len: int = 2048, needs_custom_pooler: bool = False):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         self.prot_tokenizer = AutoTokenizer.from_pretrained(model_id, output_hidden_states=True)
         self.prot_model = AutoModel.from_pretrained(model_id, output_hidden_states=True).to(device=self.device)
+
+        self.needs_custom_pooler = needs_custom_pooler
         self.max_len = max_len
     
     def __call__(self, input: str):
         tokens = self.prot_tokenizer(input, return_tensors="pt", max_length=self.max_len, padding=True, truncation=True).to(device=self.device) # TODO: Set up attention mask here
         output = self.prot_model(**tokens)
-        return output.pooler_output
+        return output.hidden_states
     
--- protCLIP/arch.py
+++ protCLIP/arch.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn, Tensor
 import torch.nn.functional as F
+from base_models.transformer import BertPooler
 import lightning as L
 from base_models.transformer import BertModel, text_model_id, prot_model_id
 from typing import Mapping, Any
@@ -15,16 +16,18 @@ class ProtCLIP(nn.Module):
         self.d_clip = d_clip
         self.d_inter = d_inter if d_inter is not None else 4 * d_clip
 
+        self.prot_pooler = BertPooler(self.d_prot)
         self.prot = MLPTransform(self.d_prot, self.d_inter, self.d_clip)
 
+        self.text_pooler = BertPooler(self.d_prot)
         self.text = MLPTransform(d_text, self.d_inter, self.d_clip)
 
         # self.norm = nn.LayerNorm(d_clip)
         self.act = nn.Sigmoid()
 
     def forward(self, input_prot: Tensor, input_text: Tensor) -> Tensor:
-        prot = self.prot(input_prot)
-        text = self.text(input_text)
+        prot = self.prot(self.prot_pooler(input_prot))
+        text = self.text(self.text_pooler(input_text))
 
         return self.act(prot @ text.t())
 
@@ -34,15 +37,17 @@ class GatedProtCLIP(nn.Module):
 
         self.prot = CrossGatedTransformer(n_enc, d_prot, d_text, d_clip, d_inter, use_gate_act)
         self.text = CrossGatedTransformer(n_enc, d_text, d_prot, d_clip, d_inter, use_gate_act)
+        self.prot_pooler = BertPooler(d_clip, use_norm=True)
+        self.text_pooler = BertPooler(d_clip, use_norm=True)
 
         self.norm = nn.LayerNorm(d_clip)
         self.act = nn.Sigmoid()
 
     def forward(self, input_prot: Tensor, input_text: Tensor) -> Tensor:
-        prot = self.prot(input_prot, input_text)
-        text = self.text(input_text, input_prot)
+        prot = self.prot_pooler(self.prot(input_prot, input_text))
+        text = self.text_pooler(self.text(input_text, input_prot))
 
-        return self.act(self.norm(prot @ text.t()))
+        return self.act(prot @ text.t())
 
 
 class ProtCLIPLit(L.LightningModule):
